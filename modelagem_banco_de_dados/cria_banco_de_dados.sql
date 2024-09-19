15deacf0d830d5bd1087ed649661f4e35f585e60
Heitor Ferreira <heitor.ferreira231203@gmail.com>
2024-09-19 01:40:35 -0300

    Inserindo modelagem do banco de dados 19.09.2024

---
+++ modelagem_banco_de_dados/cria_banco_de_dados.sql
@@ -0,0 +1,57 @@
+-- Criação do Data Base do projeto
+CREATE DATABASE b_lumitemp_main_db;
+
+USE b_lumitemp_main_db;
+
+------------------------------------
+-- Criação da tabela de cadastro de funcionários representantes de vendas ou de funcionários capazes de realizar cadastros ou manutenções nos sensores
+
+DROP TABLE cadr_func;
+
+CREATE TABLE cadr_func (
+
+    NR_USUA INT IDENTITY(1,1) PRIMARY KEY
+	,LOGIN_USUA VARCHAR(30)
+	,SENHA_USUA VARCHAR(30)
+	,DT_CADR DATE
+
+);
+
+------------------------------------
+-- Criação de tabela que guarda informações dos sensores
+
+DROP TABLE sens_info;
+
+CREATE TABLE sens_info (
+
+    CD_SENS INT IDENTITY(1, 1) PRIMARY KEY
+    ,DS_TIPO_SENS VARCHAR(30)
+    ,DT_HR_LEIT  DATETIME NOT NULL
+    ,VL_TEMP DECIMAL(5, 2) NOT NULL
+    ,VL_UMID DECIMAL(5, 2) NOT NULL
+    ,VL_TEMP_ALVO DECIMAL(5, 2)
+    ,VL_UMID_ALVO DECIMAL(5, 2)
+    ,CD_MOTOR INT
+    ,CONSTRAINT FK_NR_USUA FOREIGN KEY (NR_USUA)
+        REFERENCES cadr_func(NR_USUA)
+    ,CONSTRAINT FK_CD_EMPR FOREIGN KEY (CD_EMPR)
+        REFERENCES cadr_empr(CD_EMPR)
+
+);
+
+------------------------------------
+-- Criação de tabela que contém informações sobre as empresas parceiras das quais essas alugaram os sensores
+
+DROP TABLE cadr_empr_parc;
+
+CREATE TABLE cadr_empr_parc (
+
+    CD_EMPR IDENTITY (1, 1) PRIMARY KEY
+    ,NM_EMPR VARCHAR(30)
+    ,CEP_EMPR VARCHAR(8)
+    ,CNPJ_EMPR VARCHAR(15)
+    ,TELF_CONT_EMPR VARCHAR(10)
+    ,CONSTRAINT FK_NR_USUA FOREIGN KEY (NR_USUA)
+        REFERENCES cadr_func(NR_USUA)
+
+)
